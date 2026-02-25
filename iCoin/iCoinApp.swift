7d3ddebcdbe7fcd6a801876e72162cde07d312d7
Momeks <momeksdev@gmail.com>
2026-02-25 14:38:55 +0100

    Initial Commit

---
+++ iCoin/iCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCoinApp.swift
+//  iCoin
+//
+//  Created by Momeks on 25.02.26.
+//
+
+import SwiftUI
+
+@main
+struct iCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
